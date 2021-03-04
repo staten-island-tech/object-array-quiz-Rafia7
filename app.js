991ac0a60e6dd61aa06b2344bb6db6a4acf1a7a8
Rafia7 <raf.arnob8@gmail.com>
2021-03-03 23:44:15 -0500

    Quiz
    Did the 4 questions attempted bonus idk why it's not working.

--- app.js
+++ app.js
@@ -10,16 +10,60 @@ const presidents = [
   { name: "Richard Nixon", terms: 2, party: "R" },
   { name: "John F. Kennedy", terms: 1, party: "D" },
   { name: "Lyndon Johnson", terms: 2, party: "D" },
-  { name: "Dwight Eisenhower", terms: 2, party: "R" }
+  { name: "Dwight Eisenhower", terms: 2, party: "R" },
 ];
 
 //Challenge Questions
 //1) Filter all presidents, leaving only the Democratic ones
 
+const Democratic = presidents.filter(function (president) {
+  if (president.party === "D") {
+    return true;
+  }
+});
+
+console.log(Democratic);
+
 //2)Filter all presidents to leave only one term Republican presidents HINT use If statement
+
+const republicanSingle = presidents.filter(function (president) {
+  if (president.party === "R" && president.terms === 1) {
+    return true;
+  }
+});
+
+console.log(republicanSingle);
 
 //3) return only the last three presidents
 
+const recentPresidents = [...presidents.slice(0, 3)];
+
+console.log(recentPresidents);
+
 //4) log all dems who served 2 terms. HINT use chain filter, filter and slice
 
-//BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
+const doubleDems = presidents.filter(function (president) {
+  if (president.party === "D" && president.terms === 2) {
+    return true;
+  }
+});
+
+console.log(doubleDems);
+
+//const twoDs = [...Democratic.slice(0, doubleDems)]
+
+//BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done
+
+/*const lyndonJohnsons = [...Democratic.splice(4)];
+
+const didWin = lyndonJohnsons.find(function (lyndonJohnson) {
+  if (lyndonJohnson.terms === 2) {
+    return `LBJ served two terms`;
+  } else {
+    return `LBJ was one and done`;
+  }
+});
+
+console.log(didWin);*/
+
+/// I don't get why it doesn't return what I'm asking it to
